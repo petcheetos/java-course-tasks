717a4f7e1e01737509524192313a433a46ccf19e
Anastasia Oleynikova <whydema@gmail.com>
2023-10-04 20:55:32 +0300

    first task

--- src/main/java/edu/hw1/task0/HelloWorld.java
+++ src/main/java/edu/hw1/task0/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.hw1.task0;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class HelloWorld {
+public final class HelloWorld {
     private final static Logger LOGGER = LogManager.getLogger();
     private HelloWorld() {
     }
