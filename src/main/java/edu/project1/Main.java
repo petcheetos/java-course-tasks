4c02ada0edbaccd05bb5ca0d9020fde47cf4a8c1
Anastasia <whydema@gmail.com>
2023-10-26 11:45:38 +0300

    add Main

---
+++ src/main/java/edu/project1/Main.java
@@ -0,0 +1,12 @@
+package edu.project1;
+
+public class Main {
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        ConsoleHangman hangman = new ConsoleHangman();
+        hangman.run();
+    }
+}
