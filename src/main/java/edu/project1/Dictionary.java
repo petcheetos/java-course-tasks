1112c017dd2d496c393f12493e2d6f00abae3960
Anastasia <whydema@gmail.com>
2023-10-26 12:03:53 +0300

    add Random

--- src/main/java/edu/project1/Dictionary.java
+++ src/main/java/edu/project1/Dictionary.java
@@ -1,14 +1,15 @@
 package edu.project1;
 
+import java.util.Random;
+
 public final class Dictionary {
-    private static final int DEFAULT_SIZE = 5;
+    static final Random RANDOM = new Random();
     static String[] dictionary = new String[] {"cat", "whale", "apple", "peach", "football"};
 
     private Dictionary() {
     }
 
     public static String choseRandomWord() {
-        int index = (int) (Math.random() * DEFAULT_SIZE);
-        return dictionary[index];
+        return dictionary[RANDOM.nextInt(dictionary.length + 1)];
     }
 }
