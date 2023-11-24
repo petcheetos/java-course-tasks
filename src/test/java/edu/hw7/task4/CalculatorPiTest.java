ca3fb0b5f4a68fb7bd4a623f799a3bbdaaec2251
Anastasia <whydema@gmail.com>
2023-11-24 21:36:01 +0300

    check test

--- src/test/java/edu/hw7/task4/CalculatorPiTest.java
+++ src/test/java/edu/hw7/task4/CalculatorPiTest.java
@@ -16,19 +16,4 @@ public class CalculatorPiTest {
         double result = CalculatorPi.doMultipleThreadsAlgorithm(1000000, 8);
         assertTrue(3.13D < result && result < 3.15D);
     }
-
-    @Test
-    void testSpeed() {
-        long startTimeSingleAlg = System.currentTimeMillis();
-        double resultSingleAlg = CalculatorPi.doSingleThreadAlgorithm(100000000);
-        long endTimeSingleAlg = System.currentTimeMillis();
-        long timeElapsedSingleAlg = endTimeSingleAlg - startTimeSingleAlg;
-
-        long startTimeMultipleAlg = System.currentTimeMillis();
-        double resultMultipleAlg = CalculatorPi.doMultipleThreadsAlgorithm(100000000, 8);
-        long endTimeMultipleAlg = System.currentTimeMillis();
-        long timeElapsedMultipleAlg = endTimeMultipleAlg - startTimeMultipleAlg;
-
-        assertTrue(timeElapsedMultipleAlg < timeElapsedSingleAlg);
-    }
 }
