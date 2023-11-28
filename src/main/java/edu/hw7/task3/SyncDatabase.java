f4bd2b6d7282437a308351cf6212e9e5f33380d0
Anastasia <whydema@gmail.com>
2023-11-28 18:30:48 +0300

    checkstyle

--- src/main/java/edu/hw7/task3/SyncDatabase.java
+++ src/main/java/edu/hw7/task3/SyncDatabase.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SyncDatabase implements PersonDatabase {
     private final Map<Integer, Person> idMap;
