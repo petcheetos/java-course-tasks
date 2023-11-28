f7685290e954069689a89393a205028fd5ca822c
Anastasia <whydema@gmail.com>
2023-11-28 23:15:29 +0300

    add tests

--- src/test/java/edu/hw7/task4/CalculatorPiTest.java
+++ src/test/java/edu/hw7/task4/CalculatorPiTest.java
@@ -2,36 +2,40 @@ package edu.hw7.task4;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static java.lang.Math.abs;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorPiTest {
     private final Logger LOGGER = LogManager.getLogger();
 
-    @Test
-    void testSingleThreadAlgorithm() {
-        double result = CalculatorPi.doSingleThreadAlgorithm(1_000_000);
+    @ParameterizedTest
+    @ValueSource(ints = {10_000_000, 100_000_000, 1_000_000_000})
+    void testSingleThreadAlgorithm(int arg) {
+        double result = CalculatorPi.doSingleThreadAlgorithm(arg);
         assertTrue(3.12D < result && result < 3.16D);
         LOGGER.info("single thread accuracy = " + abs(Math.PI - result));
     }
 
-    @Test
-    void testMultipleThreadsAlgorithm() {
-        double result = CalculatorPi.doMultipleThreadsAlgorithm(1_000_000, 8);
+    @ParameterizedTest
+    @ValueSource(ints = {10_000_000, 100_000_000, 1_000_000_000})
+    void testMultipleThreadsAlgorithm(int arg) {
+        double result = CalculatorPi.doMultipleThreadsAlgorithm(arg, 8);
         assertTrue(3.12D < result && result < 3.16D);
         LOGGER.info("multi thread accuracy = " + abs(Math.PI - result));
     }
 
-    @Test
-    void performanceTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {10_000_000, 100_000_000, 1_000_000_000})
+    void performanceTest(int arg) {
         long startTimeSingleAlg = System.currentTimeMillis();
-        CalculatorPi.doSingleThreadAlgorithm(1_000_000_000);
+        CalculatorPi.doSingleThreadAlgorithm(arg);
         long endTimeSingleAlg = System.currentTimeMillis();
         long timeElapsedSingleAlg = endTimeSingleAlg - startTimeSingleAlg;
 
         long startTimeMultipleAlg = System.currentTimeMillis();
-        CalculatorPi.doMultipleThreadsAlgorithm(1_000_000_000, 8);
+        CalculatorPi.doMultipleThreadsAlgorithm(arg, 8);
         long endTimeMultipleAlg = System.currentTimeMillis();
         long timeElapsedMultipleAlg = endTimeMultipleAlg - startTimeMultipleAlg;
 
