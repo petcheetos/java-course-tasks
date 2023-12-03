12a5700cb63cb980b13c86ecc68943c48d8df70e
Anastasia <whydema@gmail.com>
2023-12-03 23:58:58 +0300

    delete unused import

--- src/test/java/edu/hw8/task2/FixedThreadPoolTest.java
+++ src/test/java/edu/hw8/task2/FixedThreadPoolTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FixedThreadPoolTest {
 
