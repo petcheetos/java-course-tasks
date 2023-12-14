a41c09bd46682a28906d76c1ddcd80befcdfd139
Anastasia <whydema@gmail.com>
2023-12-14 14:02:48 +0300

    checkstyle

--- src/main/java/edu/project4/model/Point.java
+++ src/main/java/edu/project4/model/Point.java
@@ -3,14 +3,14 @@ package edu.project4.model;
 public record Point(double x, double y) {
 
     public Point rotatePoint(double theta) {
-        double x = x() * Math.cos(theta) - y() * Math.sin(theta);
-        double y = x() * Math.sin(theta) + y() * Math.cos(theta);
-        return new Point(x, y);
+        double newX = x() * Math.cos(theta) - y() * Math.sin(theta);
+        double newY = x() * Math.sin(theta) + y() * Math.cos(theta);
+        return new Point(newX, newY);
     }
 
     public Point affineTransformPoint(Coefficient coefficient) {
-        double x = coefficient.a() * x() + coefficient.b() * y() + coefficient.c();
-        double y = coefficient.d() * x() + coefficient.e() * y() + coefficient.f();
-        return new Point(x, y);
+        double newX = coefficient.a() * x() + coefficient.b() * y() + coefficient.c();
+        double newY = coefficient.d() * x() + coefficient.e() * y() + coefficient.f();
+        return new Point(newX, newY);
     }
 }
