61f4f371968425b89f5f838d4f47fe54ca357d75
Anastasia <whydema@gmail.com>
2023-12-18 18:54:31 +0300

    checkstyle

--- src/main/java/edu/hw10/task1/generators/BooleanGenerator.java
+++ src/main/java/edu/hw10/task1/generators/BooleanGenerator.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 public class BooleanGenerator implements Generator<Boolean> {
     private final Random random;
+    private final int numberOfVariants = 3;
 
     public BooleanGenerator() {
         this.random = new Random();
@@ -18,7 +19,7 @@ public class BooleanGenerator implements Generator<Boolean> {
         if (annotation != null) {
             return random.nextBoolean();
         } else {
-            return switch (random.nextInt(3)) {
+            return switch (random.nextInt(numberOfVariants)) {
                 case 0 -> false;
                 case 1 -> true;
                 default -> null;
